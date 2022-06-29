4f33c3e14fa05050906904596daa1e94a93ce048
Theo Jolliffe <theojolliffe@gmail.com>
2022-06-29 11:27:06 +0100

    commit

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ import ObjectsToCsv from "objects-to-csv";
 
 import fs from 'fs';
 
-let outdata
-fs.readFile('./static/data/lad_list_2021.json', 'utf8' , (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    outdata = data
-    console.log(outdata[0])
+let areas = fs.readFileSync('./static/data/lad_list_2021.json', 'utf8')
+const areasobj = JSON.parse(areas);
+let reports = fs.readFileSync('./static/areareportsprgen.json', 'utf8')
+const reportsobj = JSON.parse(reports);
 
-    const obj = JSON.parse(outdata);
+areasobj.forEach(e => {
+    let article = reportsobj[e['code']].split(`<! -- Start of th article \\/-->`)[1]
+    e['standfirst'] = article.split()
+});
 
-    const csv = new ObjectsToCsv(obj)
-    const doSomething = async () => {
-        await csv.toDisk('./list.csv')
-    }
-    doSomething()
-  })
+const doSomething = async () => {
+    const csv = new ObjectsToCsv(areasobj)
+    await csv.toDisk('./list.csv')
+}
+
+doSomething()
